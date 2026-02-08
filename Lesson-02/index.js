7fbfe029f233b585efe4422a9419e2a7f9dae373
Hakidie <99023101+Hakidie@users.noreply.github.com>
2026-02-08 18:35:21 +0700

    add exercises 5, 6, 7, 8, 9

--- Lesson-02/index.js
+++ Lesson-02/index.js
@@ -1,7 +1,12 @@
 import {sumArray,
         countOccurrences,
         removeDuplicates,
-        flattenArray
+        flattenArray,
+        isSymmetric,
+        findSecondLargest,
+        sortProductsByPrice,
+        findMostExpensiveProduct,
+        groupByType
 } from './utils.js'; 
 
 // 1
@@ -15,3 +20,24 @@ console.log(removeDuplicates([1, 2, 2, 3, 4, 4, 6, 2, 3, 6]));
 
 // 4
 console.log(flattenArray([[1, 2], [3, 4]]));
+
+// 5
+console.log(isSymmetric([1, 2, 3, 2, 1]));
+
+// 6
+console.log(findSecondLargest([3, 1, 4, 2, 4]));
+
+// 7
+console.log(sortProductsByPrice([ { name: 'Product A', price: 30 },
+                                  { name: 'Product B', price: 20 },
+                                  { name: 'Product C', price: 50 } ]));
+
+// 8
+console.log(findMostExpensiveProduct([ { name: 'Product A', price: 30 },
+                                       { name: 'Product B', price: 20 },
+                                       { name: 'Product C', price: 50 } ]));
+
+// 9
+console.log(groupByType([ { type: 'fruit', name: 'apple' },
+                          { type: 'vegetable', name: 'carrot' },
+                          { type: 'fruit', name: 'banana' } ]));
--- Lesson-02/utils.js
+++ Lesson-02/utils.js
@@ -22,8 +22,80 @@ function flattenArray(arr) {
     return oneDArray;
 }
 
+// 5
+function isSymmetric(arr) {
+    let symmetric = true;
+    for(let i = 0; i < arr.length / 2; i++) {
+        if(arr[i] !== arr[arr.length - 1 - i]) {
+            symmetric = false;
+            break;
+        }
+    }
+    return symmetric;
+    // return arr.every((element, i) => {
+    //     return element === arr[arr.length - 1 - i]
+    // });
+}
+
+// 6
+function findSecondLargest(arr) {
+    let largest = arr[0];
+    let secondLargest = arr[1];
+    for(let i = 0; i < arr.length; i++) {
+        if(arr[i] > largest) {
+            secondLargest = largest;
+            largest = arr[i];
+        } 
+        else if(arr[i] > secondLargest && arr[i] < largest)
+            secondLargest = arr[i];
+    }
+    return secondLargest;
+}
+
+// 7
+function sortProductsByPrice(products) {
+    return products.sort((productA, productB) => productA.price - productB.price);
+}
+
+// 8
+function findMostExpensiveProduct(products) {
+    return products.reduce((mostExpensive, product) => {
+        return product.price > mostExpensive.price ? product : mostExpensive
+    }, products[0]);
+}
+
+// 9
+function groupByType(arr) {
+    // const group = { fruit: [], 
+    //                 vegetable: [] };
+
+    // arr.forEach((product) => {
+    //     if(product.type === "fruit") 
+    //         return group.fruit.push(product.name)
+    //     else if (product.type === "vegetable")
+    //         return group.vegetable.push(product.name)
+    // });
+
+    const group = {};
+
+    arr.forEach((product) => {
+        const type = product.type;
+        if(!group[type])
+            group[type] = [];
+
+        group[type].push(product.name);
+    });
+
+    return group;
+}
+
 export {sumArray,
         countOccurrences,
         removeDuplicates,
-        flattenArray
+        flattenArray,
+        isSymmetric,
+        findSecondLargest,
+        sortProductsByPrice,
+        findMostExpensiveProduct,
+        groupByType
 };
