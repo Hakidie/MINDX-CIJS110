7b66a0693ca5e90e0f74709a9a49888a6f109e3b
Hakidie <99023101+Hakidie@users.noreply.github.com>
2026-02-24 10:11:01 +0700

    finish Lesson-03

--- Lesson-03/Lesson-03/src/components/Column/index.jsx
+++ Lesson-03/Lesson-03/src/components/Column/index.jsx
@@ -1,14 +1,43 @@
 import './styles.css';
-// import Searchbar from '../Search_bar/index.jsx';
+import Card from '../Card/index.jsx';
 // import Button from '../Button/index.jsx';
 
 function Column() {
+    const cardsData = [
+        {   id: 1, 
+            title: "Mobile Wireframes", 
+            content: "Lên bố cục ứng dụng thích nghi cho Mobile, có thể lên cả Desktop sau khi đã hoàn thành", 
+            assigned: "MindX School", 
+            status: "3", 
+            flag: "3", 
+            deadline: "Apr 12" 
+        },
+        
+        {   id: 2,
+            title: "Mobile Wireframes", 
+            content: "Thực hiện nghiên cứu người dùng để hiểu rõ nhu cầu, thói quen và mong muốn của khách hàng mục tiêu. Từ đó, đưa ra các giải pháp phù hợp nhằm nâng cao trải nghiệm người dùng.", 
+            assigned: "Nguyễn Văn A", 
+            status: "1", 
+            flag: "3", 
+            deadline: "Apr 12" 
+        },
+        
+        {   id: 3, 
+            title: "Client Call", 
+            content: "Cuộc họp trực tuyến với khách hàng để thảo luận về dự án, cập nhật tiến độ và giải quyết các vấn đề phát sinh. Cần chuẩn bị tài liệu và câu hỏi trước cuộc họp.", 
+            assigned: "Trịnh Hồng M", 
+            status: "4", 
+            flag: "3", 
+            deadline: "Apr 2"  
+        }
+    ];
+
     return (
         <div className="column_container">
             <div className="column_header">
                 <div className="column_header_left">
                     <p className="column_header_title">Title</p>
-                    <div className="total_cards">0</div>
+                    <div className="total_cards">{cardsData.length}</div>
                 </div>
                 
                 <div className="column_header_right">
@@ -18,7 +47,17 @@ function Column() {
             </div>
 
             <div className="cards_container">
-                
+                {cardsData.map((item) => (
+                    <Card 
+                        key={item.id}
+                        title={item.title}
+                        content={item.content}
+                        assigned={item.assigned}
+                        status={item.status}
+                        flag={item.flag}
+                        deadline={item.deadline}
+                    />
+                ))}
             </div>
         </div>
     )
