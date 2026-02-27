e0379f629e0532b2d1971081409c69532e7769bc
Hakidie <99023101+Hakidie@users.noreply.github.com>
2026-02-27 16:25:02 +0700

    update save input from modal

--- Lesson-04/Lesson-04/src/components/Modal/modal.jsx
+++ Lesson-04/Lesson-04/src/components/Modal/modal.jsx
@@ -6,28 +6,68 @@ import flagGreenIcon from '../../assets/flag-green-icon.svg';
 import exitIcon from '../../assets/exit-icon.svg';
 
 const flags = [
-  { id: 'green', src: flagGreenIcon },
-  { id: 'yellow', src: flagYellowIcon },
-  { id: 'red', src: flagRedIcon }
+  { id: 1, src: flagGreenIcon },
+  { id: 2, src: flagYellowIcon },
+  { id: 3, src: flagRedIcon }
 ];
 
-function Modal({ isOpened, toggleModal }) {
+function Modal({ isOpened, toggleModal, onSave }) {
+    const [flagIndex, setFlagIndex] = useState(0);
+    const [formData, setFormData] = useState({
+        id: '',
+        title: '',
+        content: '',
+        assigned: '',
+        status: 1,
+        flag: 1,
+        deadline: ''
+    });
+
     if(isOpened == false) return null;
 
-    const [flagIndex, setFlagIndex] = useState(0);
-    const cycleFlag = () => {
-        // (0 + 1) % 3 = 1
-        // (1 + 1) % 3 = 2
-        // (2 + 1) % 3 = 0 (Back to Green!)
-        setFlagIndex((prevIndex) => (prevIndex + 1) % flags.length);
+    
+    const cycleFlag = (e) => {
+        const nextIndex = (flagIndex + 1) % flags.length;
+        const nextFlagId = flags[nextIndex].id;
+
+        // 1. Cập nhật index để đổi hình ảnh trên UI
+        setFlagIndex(nextIndex);
+
+        // 2. Cập nhật giá trị ID của cờ vào object formData
+        setFormData(prev => ({
+            ...prev,
+            flag: nextFlagId
+        }));
     };
     const currentFlag = flags[flagIndex];
+
+    
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+
+        setFormData(prev => ({
+            ...prev,
+            [name]: (name === "status" || name === "flag")
+            ? Number(value)
+            : value
+        }));
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if(!formData.title.trim()) return;
+
+        console.log("Dữ liệu cuối cùng:", formData);
+        onSave(formData);
+        toggleModal();
+    };
 
     return (
         <div className="modal_container">
             <div className='modal-header'>
                 <div className='modal-header-top'>
-                    <button className="flag-button" onClick={cycleFlag}>
+                    <button className="flag-button" type="button" onClick={cycleFlag}>
                         <img src={currentFlag.src} alt={`${currentFlag.id} priority`} />
                     </button>
                     <button className='close-button' onClick={toggleModal}>
@@ -45,7 +85,7 @@ function Modal({ isOpened, toggleModal }) {
                     <div className='form-top-left'>
                         <div className='form-input-box'>
                             <p>Title <span>*</span></p>
-                            <input type='text' placeholder='Type title of task'></input>
+                            <input type='text' placeholder='Type title of task' name="title" value={formData.title} onChange={handleChange}></input>
                             <p className='note'>Title is required</p>
                         </div>
                     </div>
@@ -53,7 +93,7 @@ function Modal({ isOpened, toggleModal }) {
                     <div className='form-top-right'>
                         <div className='form-input-box'>
                             <p>End Date</p>
-                            <input className='form-date' type='date' placeholder='15/06/2024'></input>
+                            <input className='form-date' type='date' placeholder='15/06/2024' name="deadline" value={formData.deadline} onChange={handleChange}></input>
                         </div>
                     </div>
                 </div>
@@ -62,19 +102,19 @@ function Modal({ isOpened, toggleModal }) {
                     <div className='form-top-left'>
                         <div className='form-input-box'>
                             <p>Description</p>
-                            <textarea className='form-text-area' placeholder='Type description...' row='3'></textarea>
+                            <textarea className='form-text-area' placeholder='Type description...' name="content" value={formData.content} onChange={handleChange}></textarea>
                         </div>
                     </div>
 
                     <div className='form-top-right'>
                         <div className='form-input-box'>
                             <p>Assign</p>
-                            <select className="assign-select" name='status' defaultValue="">
+                            <select className="assign-select" name='assigned' defaultValue="" value={formData.assigned || ""} onChange={handleChange}>
                                 <option value="" disabled hidden>Nguyễn Văn A</option>
-                                <option value="1">1</option>
-                                <option value="2">2</option>
-                                <option value="3">3</option>
-                                <option value="4">4</option>
+                                <option value="Nguyễn Văn A">Nguyễn Văn A</option>
+                                <option value="Trịnh Hồng M">Trịnh Hồng M</option>
+                                <option value="Lạc Khôi B">Lạc Khôi B</option>
+                                <option value="MindX School">MindX School</option>
                             </select>
                         </div>
                     </div>
@@ -82,19 +122,19 @@ function Modal({ isOpened, toggleModal }) {
 
                 <div className='form-bottom'>
                     <p>Status</p>
-                    <select className="status-select" name='status' defaultValue="">
+                    <select className="status-select" name='status' defaultValue="" value={formData.status} onChange={handleChange}>
                         <option value="" disabled hidden>Choose status</option>
-                        <option value="1">1</option>
-                        <option value="2">2</option>
-                        <option value="3">3</option>
-                        <option value="4">4</option>
+                        <option value={1}>1</option>
+                        <option value={2}>2</option>
+                        <option value={3}>3</option>
+                        <option value={4}>4</option>
                     </select>
                 </div>
             </div>
 
             <div className='buttons-container'>
                 <button className='cancel-button' onClick={toggleModal}>Cancel</button>
-                <button className='save-button'>Save</button>
+                <button className='save-button' onClick={handleSubmit}>Save</button>
             </div>
         </div>
     )
