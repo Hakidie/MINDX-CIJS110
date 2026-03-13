1f6d4d7fd6f38393752ea04123d9613dab022a67
Hakidie <99023101+Hakidie@users.noreply.github.com>
2026-03-14 00:56:11 +0700

    change styles to default

--- Midterm/Midterm/src/App.jsx
+++ Midterm/Midterm/src/App.jsx
@@ -1,14 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import './App.css'
 
+import Header from '../components/Header/Header'
+import Home from '../components/Home/Home'
+
+import { data } from '../components/Data/Data'
+
+
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
+      <Header />
+      <Home movieData={data} />
     </>
   )
 }
