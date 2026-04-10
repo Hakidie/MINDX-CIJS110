72a251a1d89e86a69fd4e3be5de6e84c625af116
Hakidie <99023101+Hakidie@users.noreply.github.com>
2026-04-10 23:20:47 +0700

    nothing

--- Final/src/App.jsx
+++ Final/src/App.jsx
@@ -1,11 +1,8 @@
-import { useState } from 'react'
 import './App.css'
 
 import ToDoPage from './pages/ToDo/ToDo';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <ToDoPage />
