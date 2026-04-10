004e518df406fb6981d2d640126fb252cf300f5b
Hakidie <99023101+Hakidie@users.noreply.github.com>
2026-04-10 23:20:24 +0700

    fix logic and add update tasks status function

--- Final/src/components/TaskCard/TaskCard.jsx
+++ Final/src/components/TaskCard/TaskCard.jsx
@@ -2,22 +2,24 @@ import './TaskCard.css';
 
 import { useState } from 'react';
 import removeTask from '../../utils/removeTab';
+import updateTask from '../../utils/updateTask';
 
 import trashIcon from '../../assets/delete.png';
 
-const TaskCard = ({ task, tasks, setTasks,activeTab }) => {
+const TaskCard = ({ task, tasks, setTasks, activeTab }) => {
     const [status, setStatus] = useState(task.active);
 
     const handleStatus = () => {
-        task.active = !status;
+        // task.active = !status;
         setStatus(!status);
         console.log(task);
+        updateTask(task, tasks, setTasks);
     };
 
     return (
         <div className='task-card-container'>
-            <input type='checkbox' checked={status} onChange={handleStatus}/>
-            <p className={`task-status ${status === true ? "finish" : ""}`}>{task.text}</p>
+            <input type='checkbox' checked={!status} onChange={handleStatus}/>
+            <p className={`task-status ${status === false ? "finish" : ""}`}>{task.text}</p>
             {activeTab === 'completed' &&
                 <button onClick={() => removeTask(task, tasks, setTasks)}><img src={trashIcon} alt='delele icon'/></button>
             }
